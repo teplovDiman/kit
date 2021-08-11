2ab3c6365a6afa36711abe635fafa79742108fc6
diman <teplov.diman@gmail.com>
2021-08-11 14:48:42 +0300

    KIT-38 |B| Add REST API Documentation

---
+++ backend/app/src/main/kotlin/com/life/kit/config/SwaggerConfiguration.kt
@@ -0,0 +1,35 @@
+package com.life.kit.config
+
+import io.swagger.v3.oas.models.ExternalDocumentation
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.info.License
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfiguration(
+
+  private val kitProperties: KitPropertiesConfiguration
+
+) {
+
+  @Bean
+  fun openAPIBean(): OpenAPI? {
+    return OpenAPI()
+      .info(
+        Info()
+          .title("REST API for Kit Application")
+          .description("REST API for Kit Application offer you whole available API to create your custom client, " +
+              "service for integration, bots or even... but let's start!")
+          .version(kitProperties.version)
+          .license(
+            License()
+              .name("No License yet :(")
+              .url("http://fakeurlblablabla.com")))
+      .externalDocs(
+        ExternalDocumentation()
+          .description("Documentation")
+          .url("https://github.com/teplovDiman/kit/wiki"))
+  }
+}
